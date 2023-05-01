04aead73b764e1382781dc6954e93149db7edd23
VovBer <vlberezhnov@yandex.ru>
2023-05-02 01:58:12 +0500

    исправил 1,4,5 задание по js

--- hw-3.js
+++ hw-3.js
@@ -1,6 +1,6 @@
 // 1
 let password = 'пароль';
-let answer = String(prompt('Введите пароль'));
+let answer = prompt('Введите пароль');
 
 if (answer === password) {
     alert('Пароль введен верно');
@@ -25,51 +25,32 @@ if (d>100 || e>100) {
 }
 // 4
 let a = '2';
-let b = '3';
-a = 2;
-b = 3;
-alert(a+b);
+let b = '3'
+alert(Nunber(a) + Number(b));
 // 5
 let monthNumber = Number(prompt('Введите номер месяца в году'));
 
 switch (monthNumber) {
     case 1:
-        alert('Зима');
-        break;
     case 2:
+    case 12:    
         alert('Зима');
         break;
     case 3:
-        alert('Весна');
-        break;
     case 4:
-        alert('Весна');
-        break;
     case 5:
         alert('Весна');
         break;
     case 6:
-        alert('Лето');
-        break;
     case 7:
-        alert('Лето');
-        break;
     case 8:
         alert('Лето');
         break;
     case 9:
-        alert('Осень');
-        break;
     case 10:
-        alert('Осень');
-        break;
     case 11:
         alert('Осень');
-        break;
-    case 12:
-        alert('Зима');
         break;                    
-
 
     default:
         alert('номер месяца больше 12')
